fbec52e9d8351837126d4f9fb09875ca667593b6
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2024-02-08 15:37:09 +0000

    Every response to the assigned tasks

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Encrypting passwords and validate password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hash password using bcrypt
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check valid password
+    validate that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
